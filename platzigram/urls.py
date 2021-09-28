c0087b799407cc42d04cb603c34098e48bd0c80f
Michel-Corona <michel.carmia@gmail.com>
2021-09-28 12:02:31 -0500

    Implementación del modelo de usuarios de Platzigram

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,11 +1,14 @@
 """platzigram URL Configuration
 """
-
+#Django
 from django.urls import path
+from django.contrib import admin
+
 from platzigram import views
 from posts import views as posts_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('hello-world/', views.hello_world),
     path('sorted/', views.sort_integers),
     path('hi/<str:name>/<int:age>/', views.say_hi),
