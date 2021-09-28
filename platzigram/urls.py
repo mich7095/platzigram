2dd190c5568197a2a4af19b3f953766b603126f5
Michel-Corona <michel.carmia@gmail.com>
2021-09-28 16:14:21 -0500

    Login

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 
 from platzigram import views
 from posts import views as posts_views
+from users import views as users_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,5 +17,7 @@ urlpatterns = [
     path('sorted/', views.sort_integers),
     path('hi/<str:name>/<int:age>/', views.say_hi),
 
-    path('posts/', posts_views.list_posts),
+    path('posts/', posts_views.list_posts, name='feed'),
+
+    path('users/login/', users_views.login_view, name='login'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
