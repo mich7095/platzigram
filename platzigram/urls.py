4444c08ca9d91a6f2d0b54a1d9388c2978e517d9
Michel-Corona <michel.carmia@gmail.com>
2021-09-28 13:22:42 -0500

    Creación del modelo de posts

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -3,6 +3,8 @@
 #Django
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from platzigram import views
 from posts import views as posts_views
@@ -14,4 +16,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', views.say_hi),
 
     path('posts/', posts_views.list_posts),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
